516298afd19bd362e40d7195e504ce969819646d
SneakyLearning <1401296232@qq.com>
2020-03-07 17:02:15 +0800

    3.7

--- leetcode.py
+++ leetcode.py
@@ -318,3 +318,155 @@ class Solution2():
         if jin == 1:
             p.next = ListNode(1)
         return head.next
+
+class Solution136():#一个数异或同一个数两次还是自身
+    def singlenumber(self,nums):
+        a = 0
+        for i in nums:
+            a ^= i
+        return  a
+
+class Solution7():
+    def reverse(self,x):
+        mark = 0
+        if x<0:mark=1
+        x=str(abs(x))
+        x=x[::-1]
+        x=int(x)
+        if -2 ** 31 < x < 2 ** 31 - 1:
+            return (x * ((-1) ** mark))
+        else:
+            return 0
+
+class Solution9():
+    def isPalindrome(self,x):
+        x=str(x)
+        for i in range(len(x)//2):
+            if not x[i] == x[-i-1]:
+                return False
+        return True
+
+class Solution14():
+    def longestCommonPrefix(self,strs):
+        pre = []
+
+class Solution994():
+    def orangesRotting(self,grid):
+        from collections import deque
+        quque = deque()
+        m,n = len(grid),len(grid[0])
+        dx = [1,-1,0,0]
+        dy = [0,0,1,-1]
+        def bfs(queue):
+            time = 0
+            while quque:
+                pos = quque.popleft()
+                i,j,time=pos
+                for k in range(4):
+                    i1=i+dx[k]
+                    j1=j+dy[k]
+                    if 0<=i1<m and 0<=j1<n:
+                        if grid[i1][j1]==1:
+                            grid[i1][j1]=2
+                            quque.append((i1,j1,time+1))
+            return time
+
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 2:
+                    quque.append((i,j,0))
+        result = bfs(quque)
+        if any(1 in row for row in grid):
+            return -1
+        return result
+
+class Solution53():
+    def brute(self,nums):
+        max_num = nums[0]
+        for i in range(len(nums)):
+            sums = 0
+            for j in range(i,len(nums)):
+                sums += nums[j]
+                if sums>max_num:
+                    max = sums
+        return max_num
+    def dp(self,nums):
+        n = len(nums)
+        max_num = nums[0]
+        for i in range(1,n):
+            if nums[i-1]>0:
+                nums[i] += nums[i-1]
+            max_num = max(max_num,nums[i])
+        return max_num
+    def greedy(self,nums):
+        max_num = nums[0]
+        sums = 0
+        for i in range(len(nums)):
+            sums += nums[i]
+            max_num = max(max_num,sums)
+            if sums<0:
+                sums = 0
+        return max_num
+    def divide(self,nums):
+        return self.helper(nums,0,len(nums)-1)
+    def helper(self,nums,left,right):
+        if left==right:
+            return nums[left]
+        p = (left+right)//2
+        left_sum = self.helper(nums,left,p)
+        right_sum = self.helper(nums,p+1,right)
+        cross_sum = self.cross_sum(nums,left,right,p)
+        return max(left_sum,right_sum,cross_sum)
+    def cross_sum(self,nums,left,right,p):
+        if left == right:
+            return nums[left]
+        left_sums = 0
+        left_max = float('-inf')
+        for i in range(p,left-1,-1):
+            left_sums += nums[i]
+            left_max = max(left_max,left_sums)
+        right_sums = 0
+        right_max = float('-inf')
+        for i in range(p+1,right+1):
+            right_sums += nums[i]
+            right_max = max(right_max, right_sums)
+        return left_max+right_max
+class Solution1103():
+    def distributeCandies(self,candies,num_people):
+        ans = [0]*num_people
+        i = 0
+        while candies:
+            ans[i%num_people] += min(i+1,candies)
+            candies -= min(i+1,candies)
+            i += 1
+        return ans
+    def anotherway(self,candies,num_people):
+        p = int((2*candies+0.25)**0.5-0.5)
+        ans = [0]*num_people
+        remain = int(candies-p*(p+1)*0.5)
+        turns = p//num_people
+        cols = p%num_people
+        for i in range(num_people):
+            ans[i] =(i + 1) * turns + int(turns * (turns - 1) * 0.5) * num_people
+            if i<cols:
+                ans[i]+=i+1+turns*num_people
+        ans[cols] += remain
+        return ans
+
+class Solution57():
+    def findContinuousSequence(self,target):
+        ans = []
+        i=1
+        j=2
+        while i<target/2:
+            if (i+j)*(j-i+1)/2 == target:
+                ans.append(list(range(i,j+1)))
+                i +=1
+            elif (i+j)*(j-i+1)/2 < target:
+                j+=1
+            elif (i+j)*(j-i+1)/2 > target:
+                i+=1
+        return ans
+
+sol = Solution57()
+print(sol.findContinuousSequence(9))
